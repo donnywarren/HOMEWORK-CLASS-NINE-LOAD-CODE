9615b67f5eea07b08e45c2600936de6d46759387
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-14 20:44:45 -0500

    fixing spin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById("red-btn").addEventListener("click",createRedBox);
   function createRedBox() {
     var newRedBox = document.createElement("div");
-    newRedBox.className="result-box red";
+    newRedBox.className="result-box red razzle-dazzle";
     document.getElementById("landing-box").appendChild(newRedBox);
     console.log("red button was clicked");
   }
@@ -13,7 +13,7 @@ document.getElementById("red-btn").addEventListener("click",createRedBox);
   document.getElementById("green-btn").addEventListener("click",createGreenBox);
     function createGreenBox() {
       var newGreenBox = document.createElement("div");
-      newGreenBox.className="result-box green";
+      newGreenBox.className="result-box green razzle-dazzle";
       document.getElementById("landing-box").appendChild(newGreenBox);
       console.log("green button was clicked")
     }
@@ -23,9 +23,19 @@ document.getElementById("red-btn").addEventListener("click",createRedBox);
   document.getElementById("blue-btn").addEventListener("click",createBlueBox);
     function createBlueBox() {
       var newBlueBox = document.createElement("div");
-      newBlueBox.className="result-box blue";
+      newBlueBox.className="result-box blue razzle-dazzle";
       document.getElementById("landing-box").appendChild(newBlueBox);
       console.log("blue button was clicked")
+    }
+
+
+
+  document.getElementById("purple-btn").addEventListener("click",createPurpleBox);
+    function createPurpleBox() {
+      var newPurpleBox = document.createElement("div");
+      newPurpleBox.className="result-box purple razzle-dazzle";
+      document.getElementById("landing-box").appendChild(newPurpleBox);
+      console.log("purple button was clicked")
     }
 
 });
